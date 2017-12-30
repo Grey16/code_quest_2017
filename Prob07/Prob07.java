5035e1a3e7abbcba9defc46d4ff7bf55dff6da69
Shiyuan Huang <shiyuancn@live.cn>
2017-12-30 16:29:43 +0000

    added sample input and output

---
+++ Prob07/Prob07.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Prob07 {
+    public static void main(String[] args) {
+        try {
+            File f = new File("Prob07.in.txt");
+            FileReader fr = new FileReader(f);
+            BufferedReader br = new BufferedReader(fr);
+            
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
