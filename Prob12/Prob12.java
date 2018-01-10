a25939598f844fcab909e3e148ae00502d55698c
Shiyuan Huang <19438967+Grey16@users.noreply.github.com>
2018-01-10 16:38:28 -0500

    refined input mechanism

--- Prob12/Prob12.java
+++ Prob12/Prob12.java
@@ -11,10 +11,30 @@ public class Prob12 {
             int n = Integer.parseInt(br.readLine());
             for(int i = 0; i < n; i++) {
                 int m = Integer.parseInt(br.readLine());
+                double balance = 0;
                 for(int j = 0; j < m; j++) {
+                    String temp = "";
                     System.out.println(j + 1);
-                    br.readLine();
+                    char[] input = br.readLine().toCharArray();
+                    boolean firstInput = true;
+                    int startIndex = String.valueOf(j).length() + 1;
+                    for(int k = startIndex; k < input.length; k++) {
+                        if(input[k] == ',') {
+                            if(temp.length() != 0) {
+                                balance += Double.parseDouble(temp);
+                                System.out.println("The current entry: " + temp + " The new balance: " + String.valueOf(balance));
+                                temp = "";
+                            }
+                        } else {
+                            temp += String.valueOf(input[k]);
+                        }
+                    }
+                    if(temp.length() != 0) {
+                        balance -= Double.parseDouble(temp);
+                        System.out.println("The current entry: " + temp + " The new balance: " + String.valueOf(balance));
+                    }
                 }
+                System.out.println((balance / m) * (0.18 / 12));
             }
         } catch(Exception e) {
             e.printStackTrace();
