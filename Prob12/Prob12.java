b2fa4b4511c0c52202ad18b4774a3d80d1cff5a2
Shiyuan Huang <19438967+Grey16@users.noreply.github.com>
2018-01-11 12:58:31 -0500

    completed implementation

--- Prob12/Prob12.java
+++ Prob12/Prob12.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.*;
+//Need to debug; Problems with double imprecision and final output
 
 public class Prob12 {
     public static void main(String[] args) {
@@ -11,18 +12,17 @@ public class Prob12 {
             int n = Integer.parseInt(br.readLine());
             for(int i = 0; i < n; i++) {
                 int m = Integer.parseInt(br.readLine());
+                double dailyBalance = 0;
                 double balance = 0;
                 for(int j = 0; j < m; j++) {
                     String temp = "";
-                    System.out.println(j + 1);
                     char[] input = br.readLine().toCharArray();
                     boolean firstInput = true;
                     int startIndex = String.valueOf(j).length() + 1;
                     for(int k = startIndex; k < input.length; k++) {
                         if(input[k] == ',') {
                             if(temp.length() != 0) {
-                                balance += Double.parseDouble(temp);
-                                System.out.println("The current entry: " + temp + " The new balance: " + String.valueOf(balance));
+                                dailyBalance += Double.parseDouble(temp);
                                 temp = "";
                             }
                         } else {
@@ -30,11 +30,14 @@ public class Prob12 {
                         }
                     }
                     if(temp.length() != 0) {
-                        balance -= Double.parseDouble(temp);
-                        System.out.println("The current entry: " + temp + " The new balance: " + String.valueOf(balance));
+                        dailyBalance -= Double.parseDouble(temp);
                     }
+                    balance += dailyBalance;
                 }
-                System.out.println((balance / m) * (0.18 / 12));
+                double output = (balance / m) * (0.18 / 12);
+                output = (double) Math.round(output * 100d) / 100d;
+                System.out.println("$" + String.valueOf(output));
+
             }
         } catch(Exception e) {
             e.printStackTrace();
