fd6217e8bc93c4f03cb13386fa645ebc557e1257
Shiyuan Huang <19438967+Grey16@users.noreply.github.com>
2018-01-18 11:12:59 -0500

    continue implementation using HashMap

--- Prob14/Prob14.java
+++ Prob14/Prob14.java
@@ -1,5 +1,9 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class Prob14 {
     public static void main(String[] args) {
@@ -9,27 +13,69 @@ public class Prob14 {
             BufferedReader br = new BufferedReader(fr);
 
             int n = Integer.parseInt(br.readLine());
-            String[] output = new String[n];
             HashMap<String, Node> map = new HashMap<String, Node>();
+            Node top = new Node("None");
+            map.put("None", top);
+
             for(int i = 0; i < n; i++) {
                 String[] currInput = br.readLine().split(",");
-                String item = currInput[0];
-                String parent = currInput[1];
+                String itemName = currInput[0];
+                String parentName = currInput[1];
+                Node parent = map.get(parentName);
+
+                Node item = new Node(itemName);
+                map.put(itemName, item);
+
+                parent.add(item);
             }
+            top.print(-1);
         } catch(Exception e) {
             e.printStackTrace();
         }
     }
 
-    // shifts strings in String array output from startIndex to the right one place
-    // returns index of first empty string in String array output
-    public static int shiftRight(String[] output, int startIndex) {
+    public static class Node implements Comparable<Node> {
+        String name;
+        ArrayList<Node> children;
 
+        // default constructor that takes the name or key as a parameter
+        public Node(String inputName) {
+            name = inputName;
+            children = new ArrayList<Node>();
+        }
+
+        // adds a child Node to the ArrayList of the parent Node
+        public void add(Node child) {
+            children.add(child);
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public int compareTo(Node otherNode) {
+            return name.compareTo(otherNode.getName());
+        }
+
+        public void print(int depth) {
+            if(depth >= 0) {
+                for(int i = 0; i < depth; i++) {
+                    System.out.print("-");
+                }
+                System.out.println(name);
+            }
+
+            if(children.size() > 0) {
+                Node[] childrenArray = new Node[children.size()];
+                childrenArray = children.toArray(childrenArray);
+
+                Arrays.sort(childrenArray);
+
+                for(Node child: childrenArray) {
+                    child.print(depth + 1);
+                }
+            }
+        }
     }
-
-    // linear search to find the index that an item should be inserted at
-
-    // create a 2-D array in which each product is listed with its name, parent, and number of dashes
-
-    // learn how to use java.util.HashMap
 }
