35aa72497cc281b91d8da4a0a7c0a37776396145
Shiyuan Huang <19438967+Grey16@users.noreply.github.com>
2018-01-31 14:04:48 -0500

    implemented Prob16 in Java

--- Prob16/Prob16.java
+++ Prob16/Prob16.java
@@ -13,6 +13,11 @@ public class Prob16 {
 
     public static boolean solutionFound;
 
+    public static final String ADDITION = "+";
+    public static final String SUBTRACTION = "-";
+    public static final String MULTIPLICATION = "*";
+    public static final String DIVISION = "/";
+
     public static void main(String[] args) {
         try {
             File f = new File("Prob16.in.txt");
@@ -53,9 +58,117 @@ public class Prob16 {
                 Arrays.fill(operatorsUsed, false);
 
                 solutionFound = false;
+                equation = new String[numOperands + numOperators];
+                fillEquation(0);
+                if(solutionFound) {
+                    System.out.println("TRUE");
+                } else {
+                    System.out.println("FALSE");
+                }
             }
         } catch(Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void fillEquation(int currentPosition) {
+        if(currentPosition == equation.length) {
+            LinkedList<String> equationList = new LinkedList<String>();
+            for(int i = 0; i < equation.length; i++) {
+                equationList.add(equation[i]);
+            }
+            solveEquation(equationList);
+            if(solutionFound) {
+                return;
+            }
+        } else {
+            if(currentPosition % 2 == 0) {
+                for(int i = 0; i < operands.length; i++) {
+                    if(!operandsUsed[i]) {
+                        equation[currentPosition] = operands[i];
+                        operandsUsed[i] = true;
+                        fillEquation(currentPosition + 1);
+                        operandsUsed[i] = false;
+                    }
+                }
+            } else {
+                for(int i = 0; i < operators.length; i++) {
+                    if(!operatorsUsed[i]) {
+                        equation[currentPosition] = operators[i];
+                        operatorsUsed[i] = true;
+                        fillEquation(currentPosition + 1);
+                        operatorsUsed[i] = false;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void solveEquation(LinkedList<String> equationList) {
+        int currentIndex = 0;
+        String currentSymbol;
+        // handles all multiplication and division
+        while(currentIndex < equationList.size()) {
+            currentSymbol = equationList.get(currentIndex);
+            if(currentSymbol.equals(MULTIPLICATION) || currentSymbol.equals(DIVISION)) {
+                String firstOperand = equationList.get(currentIndex - 1);
+                int numerator = Integer.parseInt(firstOperand);
+                int denominator = 1;
+                while(currentSymbol.equals(MULTIPLICATION) || currentSymbol.equals(DIVISION)) {
+                    equationList.remove(currentIndex);
+                    int secondOperand = Integer.parseInt(equationList.remove(currentIndex));
+                    if(currentSymbol.equals(MULTIPLICATION)) {
+                        numerator *= secondOperand;
+                    } else {
+                        denominator *= secondOperand;
+                    }
+                    if(currentIndex < equationList.size()) {
+                        currentSymbol = equationList.get(currentIndex);
+                    } else {
+                        break;
+                    }
+                }
+
+                if(denominator == 0 || numerator % denominator != 0) {
+                    return;
+                } else {
+                    int result = numerator / denominator;
+                    equationList.remove(currentIndex - 1);
+                    equationList.add(currentIndex - 1, String.valueOf(result));
+                }
+            } else {
+                currentIndex++;
+            }
+        }
+
+        currentIndex = 0;
+        while(currentIndex < equationList.size()) {
+            currentSymbol = equationList.get(currentIndex);
+            if(currentSymbol.equals(ADDITION) || currentSymbol.equals(SUBTRACTION)) {
+                int firstOperand = Integer.parseInt(equationList.get(currentIndex - 1));
+                while(currentSymbol.equals(ADDITION) || currentSymbol.equals(SUBTRACTION)) {
+                    equationList.remove(currentIndex);
+                    int secondOperand = Integer.parseInt(equationList.remove(currentIndex));
+                    if(currentSymbol.equals(ADDITION)) {
+                        firstOperand += secondOperand;
+                    } else {
+                        firstOperand -= secondOperand;
+                    }
+                    if(currentIndex < equationList.size()) {
+                        currentSymbol = equationList.get(currentIndex);
+                    } else {
+                        break;
+                    }
+                }
+                equationList.remove(currentIndex - 1);
+                equationList.add(currentIndex - 1, String.valueOf(firstOperand));
+            } else {
+                currentIndex++;
+            }
+        }
+
+        if(Integer.parseInt(equationList.pop()) == targetResult) {
+            solutionFound = true;
         }
     }
 }
