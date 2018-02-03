2a0d7daaf85e3f6e8dc123dcff4856fd9057f676
Shiyuan Huang <19438967+Grey16@users.noreply.github.com>
2018-02-03 07:13:13 -0500

    completed implementation of Prob16 using Python

--- Prob16/Prob16.py
+++ Prob16/Prob16.py
@@ -1,112 +1,174 @@
-operands = list()
-operators = list()
-equation = list()
+import java.io.*;
+import java.util.*;
 
-operandsUsed = list()
-operatorsUsed = list()
+public class Prob16 {
+    public static String[] operands;
+    public static String[] operators;
+    public static String[] equation;
 
-targetResult = 0
+    public static boolean[] operandsUsed;
+    public static boolean[] operatorsUsed;
 
-solutionFound = False
+    public static int targetResult;
 
-ADDITION = "+"
-SUBTRACTION = "-"
-MULTIPLICATION = "*"
-DIVISION = "/"
+    public static boolean solutionFound;
 
-def solveEquation(equation):
-    currentIndex = 0
-    while currentIndex < len(equation):
-        currentSymbol = equation[currentIndex]
-        if currentSymbol == MULTIPLICATION or currentSymbol == DIVISION:
-            firstOperand = equation[currentIndex - 1]
-            numerator = int(firstOperand)
-            denominator = 1
-            while currentSymbol == MULTIPLICATION or currentSymbol == DIVISION:
-                del(equation[currentIndex])
-                secondOperand = int(equation.pop(currentIndex))
-                if currentSymbol == MULTIPLICATION:
-                    numerator *= secondOperand
-                else:
-                    denominator *= secondOperand
-                if currentIndex < len(equation):
-                    currentSymbol = equation[currentIndex]
-                else:
-                    break
-            if denominator == 0 or (numerator % denominator != 0):
-                return
-            else:
-                result = int(numerator / denominator)
-                del(equation[currentIndex - 1])
-                equation.insert(currentIndex - 1, str(result))
-        else:
-            currentIndex += 1
+    public static final String ADDITION = "+";
+    public static final String SUBTRACTION = "-";
+    public static final String MULTIPLICATION = "*";
+    public static final String DIVISION = "/";
 
-    currentIndex = 0
-    while currentIndex < len(equation):
-        currentSymbol = equation[currentIndex]
-        if currentSymbol == ADDITION or currentSymbol == SUBTRACTION:
-            firstOperand = int(equation[currentIndex - 1])
-            while currentSymbol == ADDITION or currentSymbol == SUBTRACTION:
-                del(equation[currentIndex])
-                secondOperand = int(equation.pop(currentIndex))
-                if currentSymbol == ADDITION:
-                    firstOperand += secondOperand
-                else:
-                    firstOperand -= secondOperand
-                if currentIndex < len(equation):
-                    currentSymbol = equation[currentIndex]
-                else:
-                    break
-            del(equation[currentIndex - 1])
-            equation.insert(currentIndex - 1, str(firstOperand))
-        else:
-            currentIndex += 1
+    public static void main(String[] args) {
+        try {
+            File f = new File("Prob16.in.txt");
+            FileReader fr = new FileReader(f);
+            BufferedReader br = new BufferedReader(fr);
 
-    if int(equation.pop()) == targetResult:
-        global solutionFound
-        solutionFound = True
+            int numTestCases = Integer.parseInt(br.readLine());
+            String[] currInput = null;
+            String[] symbols = null;
+            int numOperands;
+            int numOperators;
+            for(int i = 0; i < numTestCases; i++) {
+                currInput = br.readLine().split(":");
+                targetResult = Integer.parseInt(currInput[0]);
 
+                symbols = currInput[1].split(" ");
+                numOperands = (symbols.length / 2) + 1;
+                numOperators = numOperands - 1;
+                operands = new String[numOperands];
+                operators = new String[numOperators];
 
-def main():
-    with open("Prob16.in.txt") as f:
-        inputList = [line.rstrip('\n') for line in f]
-        numTestCases = int(inputList[0])
-        for i in range(1, numTestCases + 1):
-            currInput = inputList[i].split(":")
-            global targetResult
-            targetResult = int(currInput[0])
+                int operandIndex = 0;
+                int operatorIndex = 0;
+                for(int j = 0; j < symbols.length; j++) {
+                    try {
+                        Integer.parseInt(symbols[j]);
+                        operands[operandIndex] = symbols[j];
+                        operandIndex++;
+                    } catch(NumberFormatException e) {
+                        operators[operatorIndex] = symbols[j];
+                        operatorIndex++;
+                    }
+                }
 
-            symbols = currInput[1].split(" ")
-            numOperands = int((len(symbols) / 2) + 1)
-            numOperators = numOperands - 1
-            global operands
-            operands = [None] * numOperands
-            global operators
-            operators = [None] * numOperators
+                operandsUsed = new boolean[numOperands];
+                operatorsUsed = new boolean[numOperators];
+                Arrays.fill(operandsUsed, false);
+                Arrays.fill(operatorsUsed, false);
 
-            operandIndex = 0
-            operatorIndex = 0
-            for j in range(len(symbols)):
-                try:
-                    int(symbols[j])
-                    operands[operandIndex] = symbols[j]
-                    operandIndex += 1
-                except ValueError:
-                    operators[operatorIndex] = symbols[j]
-                    operatorIndex += 1
+                solutionFound = false;
+                equation = new String[numOperands + numOperators];
+                fillEquation(0);
+                if(solutionFound) {
+                    System.out.println("TRUE");
+                } else {
+                    System.out.println("FALSE");
+                }
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-            global operandsUsed
-            operandsUsed = [False] * numOperands
-            global operatorsUsed
-            operatorsUsed = [False] * numOperators
+    public static void fillEquation(int currentPosition) {
+        if(currentPosition == equation.length) {
+            LinkedList<String> equationList = new LinkedList<String>();
+            for(int i = 0; i < equation.length; i++) {
+                equationList.add(equation[i]);
+            }
+            solveEquation(equationList);
+            if(solutionFound) {
+                return;
+            }
+        } else {
+            if(currentPosition % 2 == 0) {
+                for(int i = 0; i < operands.length; i++) {
+                    if(!operandsUsed[i]) {
+                        equation[currentPosition] = operands[i];
+                        operandsUsed[i] = true;
+                        fillEquation(currentPosition + 1);
+                        operandsUsed[i] = false;
+                    }
+                }
+            } else {
+                for(int i = 0; i < operators.length; i++) {
+                    if(!operatorsUsed[i]) {
+                        equation[currentPosition] = operators[i];
+                        operatorsUsed[i] = true;
+                        fillEquation(currentPosition + 1);
+                        operatorsUsed[i] = false;
+                    }
+                }
+            }
+        }
+    }
 
-            global solutionFound
-            solutionFound = False
-            equation = [None] * (numOperands + numOperators)
-            myList = ["8", "*", "4", "+", "4", "/", "4"]
-            solveEquation(myList)
+    public static void solveEquation(LinkedList<String> equationList) {
+        int currentIndex = 0;
+        String currentSymbol;
+        // handles all multiplication and division
+        while(currentIndex < equationList.size()) {
+            currentSymbol = equationList.get(currentIndex);
+            if(currentSymbol.equals(MULTIPLICATION) || currentSymbol.equals(DIVISION)) {
+                String firstOperand = equationList.get(currentIndex - 1);
+                int numerator = Integer.parseInt(firstOperand);
+                int denominator = 1;
+                while(currentSymbol.equals(MULTIPLICATION) || currentSymbol.equals(DIVISION)) {
+                    equationList.remove(currentIndex);
+                    int secondOperand = Integer.parseInt(equationList.remove(currentIndex));
+                    if(currentSymbol.equals(MULTIPLICATION)) {
+                        numerator *= secondOperand;
+                    } else {
+                        denominator *= secondOperand;
+                    }
+                    if(currentIndex < equationList.size()) {
+                        currentSymbol = equationList.get(currentIndex);
+                    } else {
+                        break;
+                    }
+                }
 
-            #fillEquation(0)
+                if(denominator == 0 || numerator % denominator != 0) {
+                    return;
+                } else {
+                    int result = numerator / denominator;
+                    equationList.remove(currentIndex - 1);
+                    equationList.add(currentIndex - 1, String.valueOf(result));
+                }
+            } else {
+                currentIndex++;
+            }
+        }
 
-if __name__ == "__main__": main()
+        currentIndex = 0;
+        while(currentIndex < equationList.size()) {
+            currentSymbol = equationList.get(currentIndex);
+            if(currentSymbol.equals(ADDITION) || currentSymbol.equals(SUBTRACTION)) {
+                int firstOperand = Integer.parseInt(equationList.get(currentIndex - 1));
+                while(currentSymbol.equals(ADDITION) || currentSymbol.equals(SUBTRACTION)) {
+                    equationList.remove(currentIndex);
+                    int secondOperand = Integer.parseInt(equationList.remove(currentIndex));
+                    if(currentSymbol.equals(ADDITION)) {
+                        firstOperand += secondOperand;
+                    } else {
+                        firstOperand -= secondOperand;
+                    }
+                    if(currentIndex < equationList.size()) {
+                        currentSymbol = equationList.get(currentIndex);
+                    } else {
+                        break;
+                    }
+                }
+                equationList.remove(currentIndex - 1);
+                equationList.add(currentIndex - 1, String.valueOf(firstOperand));
+            } else {
+                currentIndex++;
+            }
+        }
+
+        if(Integer.parseInt(equationList.pop()) == targetResult) {
+            solutionFound = true;
+        }
+    }
+}
