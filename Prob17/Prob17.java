a6790b7956bb61d420962d82a7e7acb59fcbbb04
Shiyuan Huang <19438967+Grey16@users.noreply.github.com>
2018-02-07 10:28:22 -0500

    implemented countBlanks
    Moved some methods into the Node class

--- Prob17/Prob17.java
+++ Prob17/Prob17.java
@@ -1,8 +1,8 @@
 import java.io.*;
+import java.util.*;
 
 public class Prob17 {
     public static String[][] board;
-    public static boolean myTurn;
 
     public static void main(String[] args) {
 
@@ -28,11 +28,15 @@ public class Prob17 {
                     }
                 }
 
+                boolean myTurn;
                 if(numO == numX) {
                     myTurn = true;
                 } else {
                     myTurn = false;
                 }
+
+                Node root = new Node(board, myTurn);
+                System.out.println(root.countBlanks());
 
             }
         } catch(Exception e) {
@@ -42,67 +46,96 @@ public class Prob17 {
 
     public static class Node {
         String[][] board;
+        boolean myTurn;
         ArrayList<Node> children;
 
-        public Node(String[][] inputBoard) {
+        public Node(String[][] inputBoard, boolean inputTurn) {
             board = inputBoard;
-            children = new ArraysList<Node>;
+            myTurn = inputTurn;
+            children = new ArrayList<Node>();
         }
-    }
 
-    public static String checkWin(String[][] board) {
-        for(int i = 0; i < board.length; i++) {
-            boolean rowWin = true;
-            String firstSymbolInRow = board[i][0];
-            for(String symbol : board[i]) {
-                if(!symbol.equals(firstSymbolInRow)) {
-                    rowWin = false;
+        public int countBlanks() {
+            int blanks = 0;
+            for(String[] row : board) {
+                for(String square : row) {
+                    if(square.equals("*")) {
+                        blanks++;
+                    }
+                }
+            }
+            return blanks;
+        }
+
+        public int checkWin() {
+            for(int i = 0; i < board.length; i++) {
+                boolean rowWin = true;
+                String firstSymbolInRow = board[i][0];
+                for(String symbol : board[i]) {
+                    if(!symbol.equals(firstSymbolInRow)) {
+                        rowWin = false;
+                        break;
+                    }
+                }
+                if(rowWin) {
+                    if(firstSymbolInRow.equals("X")) {
+                        return 1;
+                    } else {
+                        return -1;
+                    }
+                }
+            }
+
+            for(int i = 0; i < board[0].length; i++) {
+                boolean columnWin = true;
+                String firstSymbolInColumn = board[0][i];
+                for(int j = 0; j < board.length; j++) {
+                    if(!board[j][i].equals(firstSymbolInColumn)) {
+                        columnWin = false;
+                        break;
+                    }
+                }
+                if(columnWin) {
+                    if(firstSymbolInColumn.equals("X")) {
+                        return 1;
+                    } else {
+                        return -1;
+                    }
+                }
+            }
+
+            boolean majorDiagonalWin = true;
+            String firstSymbolInDiagonal = board[0][0];
+            for(int i = 0; i < board.length; i++) {
+                if(!board[i][i].equals(firstSymbolInDiagonal)) {
+                    majorDiagonalWin = false;
                     break;
                 }
             }
-            if(rowWin) {
-                return firstSymbolInRow;
+            if(majorDiagonalWin) {
+                if(firstSymbolInDiagonal.equals("X")) {
+                    return 1;
+                } else {
+                    return -1;
+                }
             }
-        }
 
-        for(int i = 0; i < board[0].length; i++) {
-            boolean columnWin = true;
-            String firstSymbolInColumn = board[0][i];
-            for(int j = 0; j < board.length; j++) {
-                if(!board[j][i].equals(firstSymbolInColumn)) {
-                    columnWin = false;
+            boolean minorDiagonalWin = false;
+            firstSymbolInDiagonal = board[0][2];
+            for(int i = 0; i < board.length; i++) {
+                if(!board[i][2 - i].equals(firstSymbolInDiagonal)) {
+                    minorDiagonalWin = false;
                     break;
                 }
             }
-            if(columnWin) {
-                return firstSymbolInColumn;
+            if(majorDiagonalWin) {
+                if(firstSymbolInDiagonal.equals("X")) {
+                    return 1;
+                } else {
+                    return -1;
+                }
             }
+            return 0;
         }
-
-        boolean majorDiagonalWin = true;
-        String firstSymbolInDiagonal = board[0][0];
-        for(int i = 0; i < board.length; i++) {
-            if(!board[i][i].equals(firstSymbolInDiagonal)) {
-                majorDiagonalWin = false;
-                break;
-            }
-        }
-        if(majorDiagonalWin) {
-            return firstSymbolInDiagonal;
-        }
-
-        boolean minorDiagonalWin = false;
-        firstSymbolInDiagonal = board[0][2];
-        for(int i = 0; i < board.length; i++) {
-            if(!board[i][2 - i].equals(firstSymbolInDiagonal)) {
-                minorDiagonalWin = false;
-                break;
-            }
-        }
-        if(majorDiagonalWin) {
-            return firstSymbolInDiagonal;
-        }
-
-        return "Tie";
     }
 }
